62a4da43650f1d6b7f9e9fad1d1c324374159871
takutoUsami <u.takuto@gmail.com>
2013-02-26 10:01:07 +0900

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,6 @@
 
 int main() {
 	ofAppGlutWindow window;
-//	ofSetupOpenGL(&window, 1280, 256, OF_WINDOW);
-//	ofSetupOpenGL(&window, 800, 600, OF_WINDOW);
-//	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
-	ofSetupOpenGL(&window, 1280, 720, OF_FULLSCREEN);
-//	ofSetupOpenGL(&window, 1920, 1080, OF_WINDOW);
+	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
 	ofRunApp(new testApp());
 }
